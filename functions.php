5d1f7f834992ee8fda5e7e4dda5b2551dc646b15
jake whiteley <odegramusic@gmail.com>
2016-12-12 19:22:08 +0000

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * An additional function which allows a non OOP initialization of the Set object
+ *
+ * @param  mixed ...$args Values to initially add to the Set object
+ */
+function set() 
+{
+    return ( new \ReflectionClass('Set') )->newInstanceArgs( func_get_args() );
+}
