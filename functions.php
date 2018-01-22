1f5d1aa45351704cddf5760eea125248a1057ab6
jake whiteley <odegramusic@gmail.com>
2018-01-22 14:13:23 +0000

    Standards update
    Move to PSR2

--- functions.php
+++ functions.php
@@ -10,31 +10,30 @@
  * @param  mixed ...$args Values to initially add to the Set object
  * @return \JakeWhiteley\PhpSets\Set
  */
-function set() 
-{
-    return ( new \ReflectionClass('\JakeWhiteley\PhpSets\Set') )->newInstanceArgs( func_get_args() );
+function set() {
+	return ( new \ReflectionClass( '\JakeWhiteley\PhpSets\Set' ) )->newInstanceArgs( func_get_args() );
 }
 
 /**
  * Returns a new Set which contains the unique items of both sets
- * 
+ *
  * @param  \JakeWhiteley\PhpSets\Set $set           The original set
  * @param  \JakeWhiteley\PhpSets\Set $additionalSet The set to append
  * @return \JakeWhiteley\PhpSets\Set A new set containing the merged items
  */
-function set_merge( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet )
-{
+function set_merge( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet ) {
 	$iterator = $additionalSet->entries();
 
-	// create a copy of $set 
+	// create a copy of $set
 	$merged = new \JakeWhiteley\PhpSets\Set();
 	$merged->exchangeArray( $set->values() );
 
 	// add values from $additionalSet if not present
 	while ( $iterator->valid() ) {
-	    if ( ! $set->has( $iterator->current() ) )
-	    	$merged->add( $iterator->current() );
-	    $iterator->next();
+		if ( ! $set->has( $iterator->current() ) ) {
+			$merged->add( $iterator->current() );
+		}
+		$iterator->next();
 	}
 	$iterator->rewind();
 
@@ -43,20 +42,20 @@ function set_merge( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $a
 
 /**
  * Returns a new Set object containing the common elements between two given sets
- * 
- * @param  \JakeWhiteley\PhpSets\Set $set           
+ *
+ * @param  \JakeWhiteley\PhpSets\Set $set
  * @param  \JakeWhiteley\PhpSets\Set $additionalSet
  * @return \JakeWhiteley\PhpSets\Set
  */
-function set_intersect( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet )
-{
+function set_intersect( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet ) {
 	$iterator = $additionalSet->entries();
 	$intersect = new \JakeWhiteley\PhpSets\Set;
 
 	while ( $iterator->valid() ) {
-	    if ( $set->has( $iterator->current() ) )
-	    	$intersect->add( $iterator->current() );
-	    $iterator->next();
+		if ( $set->has( $iterator->current() ) ) {
+			$intersect->add( $iterator->current() );
+		}
+		$iterator->next();
 	}
 	$iterator->rewind();
 
@@ -67,29 +66,30 @@ function set_intersect( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Se
  * Returns a new Set containing all uncommon items between the two given Sets
  *
  * @todo  This is not very efficient as it iterates both Sets completely
- * 
- * @param  \JakeWhiteley\PhpSets\Set $set           
+ *
+ * @param  \JakeWhiteley\PhpSets\Set $set
  * @param  \JakeWhiteley\PhpSets\Set $additionalSet
  * @return \JakeWhiteley\PhpSets\Set
  */
-function set_diff( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet )
-{
+function set_diff( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet ) {
 	$originalArray = $set->entries();
 	$iterator = $additionalSet->entries();
 	$intersect = new \JakeWhiteley\PhpSets\Set;
 
 	// check $set values
 	while ( $originalArray->valid() ) {
-	    if ( ! $additionalSet->has( $originalArray->current() ) ) 
-	    	$intersect->add( $originalArray->current() );
-	    $originalArray->next();
+		if ( ! $additionalSet->has( $originalArray->current() ) ) {
+			$intersect->add( $originalArray->current() );
+		}
+		$originalArray->next();
 	}
 
 	// check $additionalSet values
 	while ( $iterator->valid() ) {
-	    if ( ! $set->has( $iterator->current() ) ) 
-	    	$intersect->add( $iterator->current() );
-	    $iterator->next();
+		if ( ! $set->has( $iterator->current() ) ) {
+			$intersect->add( $iterator->current() );
+		}
+		$iterator->next();
 	}
 	// reset both Set internal pointers
 	$originalArray->rewind();
@@ -102,22 +102,22 @@ function set_diff( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $ad
 /**
  * Checks if a given $additionalSet is a subset of $set
  * All values should be present, but ordinality does not matter
- * 
+ *
  * @param  \JakeWhiteley\PhpSets\Set $set           The original Set to check against
  * @param  \JakeWhiteley\PhpSets\Set $additionalSet The subset
  * @return bool Whether $additionalSet was a subset of $set
  */
-function set_subset( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet )
-{
+function set_subset( \JakeWhiteley\PhpSets\Set $set, \JakeWhiteley\PhpSets\Set $additionalSet ) {
 	$iterator = $additionalSet->entries();
-	
+
 	// iterate through $additionalSet and return false is an uncommon value is present
 	while ( $iterator->valid() ) {
-	    if ( ! $set->has( $iterator->current() ) )
-	    	return false;
-	    $iterator->next();
+		if ( ! $set->has( $iterator->current() ) ) {
+			return false;
+		}
+		$iterator->next();
 	}
 	$iterator->rewind();
-	
+
 	return true;
 }
--- src/set.php
+++ src/set.php
@@ -1,163 +1,172 @@
 <?php
 
-namespace JakeWhiteley\PhpSets;
+namespace PhpSets;
 
 /**
  * PHP implementation of a Java like Set object
  *
  * Stores one dimensional unique data
- * 
+ *
  * @author Jake Whiteley <https://github.com/jakewhiteley>
  * @version 1.0
  * @license http://www.gnu.org/licenses/gpl-3.0.en.html
  */
 class Set extends \ArrayObject {
-    /**
-     * The current amount of values in the set.
-     * @var integer
-     */
-    public $size = 0;
+	/**
+	 * The current amount of values in the set.
+	 *
+	 * @var integer
+	 */
+	public $size = 0;
 
-    /**
-     * Create new Set
-     *
-     * @param mixed ...$args Any number of items to add to the Set object
-     */
-    public function __construct () {
-        parent::__construct( [], \ArrayObject::STD_PROP_LIST );
-        foreach ( func_get_args() as $insert ) {
-            $this->add( $insert );
-        }
-    }
+	/**
+	 * Create new Set
+	 *
+	 * @param mixed ...$args Any number of items to add to the Set object
+	 */
+	public function __construct() {
+		parent::__construct( [], \ArrayObject::STD_PROP_LIST );
 
-    /**
-     * Appends a new element with the given value to the Set object. Returns the Set object.
-     * 
-     * @param mixed $value Value to add
-     * @return Set
-     */
-    public function add( $value ) {
-        $this->append( $value );
-        return $this;
-    }
+		foreach ( func_get_args() as $insert ) {
+			$this->add( $insert );
+		}
+	}
 
-    /**
-     * Removes all elements from the Set object. Returns the Set object.
-     * 
-     * @return Set
-     */
-    public function clear() {
-        $this->exchangeArray( [] );
-        $this->size = 0;
-        return $this;
-    }
+	/**
+	 * Appends a new element with the given value to the Set object. Returns the Set object.
+	 *
+	 * @param mixed $value Value to add
+	 * @return Set
+	 */
+	public function add( $value ) {
+		$this->append( $value );
 
-    /**
-     * Removes the element associated to the value.
-     * 
-     * @param  mixed $value The value to remove from the Set object
-     * @return boolean      True on success, false on failure to delete value
-     */
-    public function delete( $value ) {
-        $key = array_search( $value, $this->getArrayCopy(), true );
-        if ( $key !== false ) {
-            unset($this[$key]);
-            return true;
-        }
-        return false;
-    }
+		return $this;
+	}
 
-    /**
-     * Calls $callback once for each value present in the Set object, in insertion order.
-     * 
-     * Any number of additional arguments can be passed to the callback.
-     * 
-     * @param  callable $callback The callback function
-     *                            The callback is called with argument 1 being the current iterated value
-     * @param  mixed    ...$args  Additional arguments to pass to the callback function
-     * @return ArrayIterator
-     */
-    public function each( $callback ) {
-        $vars = func_get_args();
+	/**
+	 * Removes all elements from the Set object. Returns the Set object.
+	 *
+	 * @return Set
+	 */
+	public function clear() {
+		$this->exchangeArray( [] );
+		$this->size = 0;
 
-        $iterator = $this->entries();
-        while ( $iterator->valid() ) {
-            unset( $vars[0] );
-            array_unshift( $vars, $iterator->current() );
-            call_user_func_array( $callback, $vars );
-            $iterator->next();
-        }
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Returns a new ArrayIterator object that contains an array of each element in the Set object, in insertion order.
-     *
-     * @see http://php.net/manual/en/class.arrayiterator.php
-     * @return ArrayIterator
-     */
-    public function entries() {
-        return $this->getIterator();
-    }
+	/**
+	 * Removes the element associated to the value.
+	 *
+	 * @param  mixed $value The value to remove from the Set object
+	 * @return boolean      True on success, false on failure to delete value
+	 */
+	public function delete( $value ) {
+		$key = array_search( $value, $this->getArrayCopy(), true );
 
-    /**
-     * Returns a boolean asserting whether an element is present with the given value in the Set object or not.
-     * 
-     * @param  mixed  $value The value to check for.
-     * @return boolean        
-     */
-    public function has( $value ) {
-        return array_search( $value, $this->getArrayCopy(), true ) !== false ;
-    }   
+		if ( $key !== false ) {
+			unset( $this[ $key ] );
+			return true;
+		}
 
-    /**
-     * Returns an array of the values values with the Set onject
-     * 
-     * @return array
-     */
-    public function values() {
-        return $this->getArrayCopy();
-    }
+		return false;
+	}
 
-    /**
-     * The main setter for the Array functionality.
-     *
-     * Ensures duplicates cant exist and updates the size property on value insert
-     * 
-     * @param  int|null $offset  The key to insert a value at
-     * @param  mixed    $value   The value to insert
-     * @return void
-     */
-    public function offsetSet( $offset, $value ) {
-        $temp = $this->values();
+	/**
+	 * Calls $callback once for each value present in the Set object, in insertion order.
+	 *
+	 * Any number of additional arguments can be passed to the callback.
+	 *
+	 * @param  callable $callback The callback function
+	 *                            The callback is called with argument 1 being the current iterated value
+	 * @param  mixed    ...$args  Additional arguments to pass to the callback function
+	 * @return ArrayIterator
+	 */
+	public function each( $callback ) {
+		$vars = func_get_args();
+
+		$iterator = $this->entries();
+
+		while ( $iterator->valid() ) {
+			unset( $vars[0] );
+			array_unshift( $vars, $iterator->current() );
+			call_user_func_array( $callback, $vars );
+			$iterator->next();
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Returns a new ArrayIterator object that contains an array of each element in the Set object, in insertion order.
+	 *
+	 * @see http://php.net/manual/en/class.arrayiterator.php
+	 * @return ArrayIterator
+	 */
+	public function entries() {
+		return $this->getIterator();
+	}
+
+	/**
+	 * Returns a boolean asserting whether an element is present with the given value in the Set object or not.
+	 *
+	 * @param  mixed $value The value to check for.
+	 * @return boolean
+	 */
+	public function has( $value ) {
+		return array_search( $value, $this->getArrayCopy(), true ) !== false ;
+	}
+
+	/**
+	 * Returns an array of the values values with the Set onject
+	 *
+	 * @return array
+	 */
+	public function values() {
+		return $this->getArrayCopy();
+	}
+
+	/**
+	 * The main setter for the Array functionality.
+	 *
+	 * Ensures duplicates cant exist and updates the size property on value insert
+	 *
+	 * @param  int|null $offset  The key to insert a value at
+	 * @param  mixed    $value   The value to insert
+	 * @return void
+	 */
+	public function offsetSet( $offset, $value ) {
+		$temp = $this->values();
+
+		if ( $this->has( $value ) === false ) {
+			if ( is_null( $offset ) ) {
+				$temp[] = $value;
+			} else {
+				$temp[ $offset ] = $value;
+			}
+		}
+
+		$this->exchangeArray( $temp );
+
+		$this->size = $this->count();
+	}
+
+	/**
+	 * The main UNsetter for the Array functionality.
+	 *
+	 * @param  int $offset  The key to remove a value at
+	 * @return void
+	 */
+	public function offsetUnset( $offset ) {
+		$temp = $this->values();
+
+		if ( isset( $temp[ $offset ] ) ) {
+			unset( $temp[ $offset ] );
+		}
+
+		$this->exchangeArray( array_values( $temp ) );
         
-        if ( $this->has( $value ) === false ) {
-            if ( is_null($offset) ) { 
-                $temp[] = $value;
-            }
-            else {
-                $temp[$offset] = $value;
-            }
-        }
-        
-        $this->exchangeArray($temp);
-        $this->size = $this->count();
-    }
-
-    /**
-     * The main UNsetter for the Array functionality.
-     * 
-     * @param  int $offset  The key to remove a value at
-     * @return void
-     */
-    public function offsetUnset( $offset ) {
-        $temp = $this->values();
-        
-        if ( isset($temp[$offset]) ) {
-            unset($temp[$offset]);
-        }
-        
-        $this->exchangeArray( array_values($temp) );
-        $this->size = $this->count();
-    }
+		$this->size = $this->count();
+	}
 }
